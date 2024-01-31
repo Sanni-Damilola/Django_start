d86acf2a0727060735c284132930e41414032658
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-31 17:00:11 +0100

    update

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    age = models.IntegerField()
+    bioGraphy = models.TextField()
+
+    def __str__(self) -> str:
+        return f"{self.firstName}{self.lastName}"
+        
