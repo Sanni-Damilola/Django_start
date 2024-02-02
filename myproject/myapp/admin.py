bf803c67edc016efcd6453c0de627d52c44998cd
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-02 10:33:30 +0100

    added

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Teacher, Course, Candidate
 
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Course)
+admin.site.register(Candidate)
