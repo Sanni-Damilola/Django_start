af68d9fd5c034ffa0f28e34515f2b055e7322e90
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-02 11:18:32 +0100

    feat: Add user management app

--- myproject/userarea/views.py
+++ myproject/userarea/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
 
 def register(req):
+    if req.method == "POST":
+        name = req.POST["name"]
+        email = req.POST["email"]
+        password = req.POST["password"]
+        confirmpassword = req.POST["confirmpassword"]
+
+        if password == confirmpassword:
+            if User.objects.filter(email):
+                messages.info(req, "Email already exists")
+            else:
+                # create user
+                user = User.objects.create(
+                    name = name,
+                    email = email,
+                    password = password,
+                )  
+
+                user.save()
+                return redirect("login")
+        return render(req, "register.html")
+            
     return render()
