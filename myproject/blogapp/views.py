f0111b7a9301581b588b255b4edf77027e193b29
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-02 13:08:36 +0100

    feat: Create and integrate blog app

--- myproject/blogapp/views.py
+++ myproject/blogapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
 def archive(req):
-    return render(req, "blog/archive.html")
+    blogs = BlogPost.objects.all()
+    data = {
+        "blogs": blogs
+    }
+    return render(req, "blog/archive.html", data)
 
-def details(req):
-    return render(req, "blog/details.html")
+def details(req, pk):
+    blogs = BlogPost.objects.get(pk=pk)
+    return render(req, "blog/details.html", {"blogs": blogs})
