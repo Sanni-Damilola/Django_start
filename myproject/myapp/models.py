b4f57ee2b296dfcae622fe70ef3ba899da5793cb
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-02 10:25:40 +0100

    added

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -10,3 +10,19 @@ class Student(models.Model):
     def __str__(self) -> str:
         return f"{self.firstName}{self.lastName}"
         
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+    
