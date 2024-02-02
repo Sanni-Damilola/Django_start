25b04acad684564cc89bd78966baca483e9c5917
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-02 12:58:35 +0100

    feat: Add user management app

---
+++ myproject/blogapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import archive, details
+
+
+urlpatterns = [
+    path("archive", archive, name="archive"),
+    path("detail/<int:pk>", details, name="details")
+]
