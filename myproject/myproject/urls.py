b2b3c12ac0092e68efbc7e5dab0dde1801539d42
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-02 10:56:41 +0100

    feat: Add user management app

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls'))
+    path('', include('myapp.urls')),
+    path('user', include('myapp.urls'))
 ]
