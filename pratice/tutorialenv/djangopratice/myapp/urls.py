fd0a9bb43ca490616727482c9bf81a4a7795769f
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-14 12:27:24 +0100

    pratice

--- pratice/tutorialenv/djangopratice/myapp/urls.py
+++ pratice/tutorialenv/djangopratice/myapp/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import index
 
-
-
 urlpatterns = [
-    path("/", index, name="index")
+    path("", index, name="index")
 ]
--- pratice/tutorialenv/djangopratice/myapp/views.py
+++ pratice/tutorialenv/djangopratice/myapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
-
-def index(req):
-    return HttpResponse("Hello")
+def index(request):
+    return JsonResponse({"message": "Hello"})
