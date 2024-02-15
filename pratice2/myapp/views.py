c650327ace13b5f4ef8b30fed37c4ba986e23206
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-15 14:41:22 +0100

    start pratice 2

--- pratice2/myapp/views.py
+++ pratice2/myapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import MyModel
+from django.http import HttpResponse, JsonResponse
+import json
+
 
 # Create your views here.
+def getAllUsers(req):
+    if req.method == "GET":
+        data = MyModel.objects.all()
+        return JsonResponse({'data': list(data)})
+    else:
+        return JsonResponse({'error': "Invalid Method"})
+    
+
+def create(req):
